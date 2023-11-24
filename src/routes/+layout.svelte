e77a51c39e777008b2f9829a3f82157019371479
Jon <ulyger23almaty@gmail.com>
2023-11-24 15:24:22 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "../app.css";
+    import { goto } from '$app/navigation';
+
+    function navigateToSignUp() {
+    goto('/users/new');
+    }
+
+    function navMain() {
+    goto('/');
+    }
+
+    function navAbout() {
+    goto('/about');
+    }
+  </script>
+
+<button on:click={navMain}>Main Page</button>
+<button on:click={navigateToSignUp}>Sign Up</button>
+
+  <slot />
